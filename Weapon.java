416d58be2ac9cb956f1296008efc8aaacb94cc00
calebjm288 <calebjm288@ksu.edu>
2020-11-15 20:41:36 -0600

    Adding Weapon class

---
+++ Weapon.java
@@ -0,0 +1,42 @@
+public class Weapon {
+	
+	String weaponType;
+	double damage;
+	int ammoRemaining;
+	
+	
+	public Weapon(String wT, double d, int aR) {
+		weaponType = wT:
+		damage = d;
+		ammoRemaining = aR;
+	}
+	
+	public double fireWeapon(Ship target) {
+		target.takeDamage(damage);
+		ammoRemaining--;
+	}
+	
+	public String getWeaponType() {
+		return weaponType;
+	}
+	
+	public double getDamage() {
+		return damage;
+	}
+	
+	public int getAmmoRemaining() {
+		return ammoRemaining;
+	}
+	
+	public void setWeaponType(String wT) {
+		weaponType = wT;
+	}
+	
+	public void setDamage(Double d) {
+		damage = d;
+	}
+	
+	public void setAmmoRemaining(int aR) {
+		ammoRemaining = aR;
+	}
+}
