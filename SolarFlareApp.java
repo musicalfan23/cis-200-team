2d67a8fd7dcf02bd4aac92f20e6c9f368b1681e7
Matthew Gates <mgates@ksu.edu>
2020-11-18 16:09:20 +0000

    Added repl.it support

--- SolarFlareApp.java
+++ SolarFlareApp.java
@@ -1,10 +1,10 @@
 
-public class SolarFlareApp.java{
+public class SolarFlareApp{
 
 	public static void main(String[]args){
 		
 		Ship playerShip = new Ship(0,0,5);
-		
+		System.out.println("Hello");
 		
 		
 	}
